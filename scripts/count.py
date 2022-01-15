55228417703b884d2e41b5685e767795729b7ef0
Reo Yatabe <Reo Yatabe>
2022-01-15 19:49:37 +0000

    updatecount.py

--- scripts/count.py
+++ scripts/count.py
@@ -8,9 +8,9 @@ from std_msgs.msg import Int32
 
 rospy.init_node('count')
 pub = rospy.Publisher('count_up',Int32,queue_size=1)
-rate = rospy.Rate(10)
+rate = rospy.Rate(5)
 n = 0
 while not rospy.is_shutdown():
-    n += 1
+    n += 2
     pub.publish(n)
     rate.sleep()
