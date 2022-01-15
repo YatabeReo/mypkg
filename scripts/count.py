d78415df648b0e9f3e62cbaa7e9f594770271c44
Reo Yatabe <Reo Yatabe>
2022-01-15 19:15:10 +0000

    update count.py

--- scripts/count.py
+++ scripts/count.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
+#SPDX-License-Identifier:BSD-2.0
+
+#*Copyright(c)2021 Ryuich Ueda. All rights reserved.
 import rospy
 from std_msgs.msg import Int32
 
 rospy.init_node('count')
 pub = rospy.Publisher('count_up',Int32,queue_size=1)
-rate#! = rospy.Rate(10)
+rate = rospy.Rate(10)
 n = 0
 while not rospy.is_shutdown():
     n += 1
